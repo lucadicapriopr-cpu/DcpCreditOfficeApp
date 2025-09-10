5e753235e8fcaf205414d2aaeb7696334f3aa8fe
lucadicapriopr-cpu <lucadicaprio.pr@gmail.com>
2025-09-10 09:47:51 +0200

    update vite.config.js
    cambiato da localhost a onrender

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,18 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-  port: 5173,
-  strictPort: true,
-  proxy: {
-    '/api': { target: 'http://localhost:4000', changeOrigin: true },
+    port: 5173,
+    strictPort: true,
+    proxy: {
+      "/api": {
+        target: "https://dcpcreditofficeapp.onrender.com",
+        changeOrigin: true,
+        secure: true
+      }
+    }
   },
-},
   build: {
     outDir: "dist",
-    sourcemap: true,
-  },
+    sourcemap: true
+  }
 });
